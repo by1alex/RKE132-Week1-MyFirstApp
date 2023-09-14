5b14c3a160e3483784c6a4641c382113124edee2
Aleksandr Bychkov <Aleksandr.bychkov@tktk.ee>
2023-09-14 20:58:30 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,11 @@
+﻿//This is a comment to my code
+
+
+Console.WriteLine("Enter your name:");
+// string - sone
+string userName = Console.ReadLine();
+
+
+Console.WriteLine("Hello, " + userName + "!");
+
+Console.WriteLine($"Hello {userName} !");
